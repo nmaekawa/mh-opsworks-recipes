ac748b70a07d52ae9870895695585777d9b87eb8
Dan Collis-Puro <dan@collispuro.net>
2015-11-16 16:03:32 -0500

    Additional debugging

--- libraries/default.rb
+++ libraries/default.rb
@@ -113,11 +113,14 @@ module MhOpsworksRecipes
       # node comes online.
 
       cloudfront_url = get_cloudfront_url
-      if cloudfront_url
+      if ! cloudfront_url.empty?
+        Chef::Log.info "Cloudfront url: #{cloudfront_url}"
         %Q|https://#{cloudfront_url}|
       elsif using_asset_server?
+        Chef::Log.info "asset server url: #{get_public_asset_server_hostname}"
         %Q|http://#{get_public_asset_server_hostname}/static|
       else
+        Chef::Log.info "engage server: #{engage_hostname}"
         protocol = (using_ssl_for_engage?) ? 'https://' : 'http://'
         %Q|#{protocol}#{engage_hostname}/static|
       end
