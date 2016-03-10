9f541bd05dc2fbc2c97d41fb9115760eb7ed686d
nmaekawa <nmaekawa@g.harvard.edu>
2016-03-10 15:59:01 -0500

    matt-1875: consistency with double-quotes

--- recipes/create-ca-webapp-directories.rb
+++ recipes/create-ca-webapp-directories.rb
@@ -2,14 +2,14 @@
 # Recipe:: create-python-webapps-directories
 
 [
-  '/home/web/sites',
-  '/home/web/sock',
-  '/home/web/logs'
+  "/home/web/sites",
+  "/home/web/sock",
+  "/home/web/logs"
 ].each do |webapps_directory|
   directory webapps_directory do
-    owner 'web'
-    group 'web'
-    mode '755'
+    owner "web"
+    group "web"
+    mode "755"
     recursive true
   end
 end
