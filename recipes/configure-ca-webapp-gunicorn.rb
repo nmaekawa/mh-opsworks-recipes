029a54f68b8c400bce09a7b43decf0b97bc04848
nmaekawa <nmaekawa@g.harvard.edu>
2016-03-10 15:59:01 -0500

    matt-1875: consistency with double-quotes

--- recipes/configure-ca-webapp-gunicorn.rb
+++ recipes/configure-ca-webapp-gunicorn.rb
@@ -4,26 +4,26 @@
 ::Chef::Recipe.send(:include, MhOpsworksRecipes::RecipeHelpers)
 
 ca_webapp_info = node.fetch(:ca_webapp, {})
-app_name = ca_webapp_info.fetch(:ca_webapp_name, 'webapp')
+app_name = ca_webapp_info.fetch(:ca_webapp_name, "webapp")
 
-execute 'install gunicorn' do
-  command 'source /home/web/sites/#{app_name}/venv/bin/activate && pip install gunicorn'
-  user 'web'
-  creates '/home/web/sites/#{app_name}/venv/bin/gunicorn'
+execute "install gunicorn" do
+  command "source /home/web/sites/#{app_name}/venv/bin/activate && pip install gunicorn"
+  user "web"
+  creates "/home/web/sites/#{app_name}/venv/bin/gunicorn"
 end
 
-template '/home/web/sites/#{app_name}/gunicorn_start.sh' do
-  source 'ca-webapp-gunicorn_start.sh.erb'
-  owner 'web'
-  group 'web'
-  mode '775'
+template "/home/web/sites/#{app_name}/gunicorn_start.sh" do
+  source "ca-webapp-gunicorn_start.sh.erb"
+  owner "web"
+  group "web"
+  mode "775"
   variables({
-    ca_webapp_name: '#{app_name}'
+    ca_webapp_name: app_name
   })
 end
 
-directory '/home/web/sock' do
-  owner 'web'
-  group 'web'
-  mode '775'
+directory "/home/web/sock" do
+  owner "web"
+  group "web"
+  mode "775"
 end
