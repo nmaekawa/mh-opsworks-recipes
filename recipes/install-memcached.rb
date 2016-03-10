1dfe058bd15999b2e17f974be28fca3d3cb0e7e0
nmaekawa <nmaekawa@g.harvard.edu>
2016-03-10 09:00:38 -0500

    fix sed path in install-memcached recipe

--- recipes/install-memcached.rb
+++ recipes/install-memcached.rb
@@ -7,7 +7,7 @@ install_package('memcached')
 ca_webapp_info = node.fetch(:ca_webapp, {memcached_port: '8008'})
 
 execute 'memcached not in default port' do
-  command %Q|/usr/bin/sed -i .bak "s/-p 11211/-p #{ca_webapp_info[:memcached_port]}/g" /etc/memcached.conf|
+  command %Q|/bin/sed -i .bak "s/-p 11211/-p #{ca_webapp_info[:memcached_port]}/g" /etc/memcached.conf|
 end
 
 execute 'service memcached restart'
