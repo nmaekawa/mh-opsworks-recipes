6c838ac3ed706a3b7b0e8ee1c18c1be7e14763e3
nmaekawa <nmaekawa@g.harvard.edu>
2016-03-10 15:59:01 -0500

    matt-1875: consistency with double-quotes

--- recipes/install-ca-webapp.rb
+++ recipes/install-ca-webapp.rb
@@ -4,18 +4,18 @@
 ::Chef::Recipe.send(:include, MhOpsworksRecipes::RecipeHelpers)
 
 ca_webapp_info = node.fetch(:ca_webapp, {})
-app_name = ca_webapp_info.fetch(:ca_webapp_name, 'webapp')
+app_name = ca_webapp_info.fetch(:ca_webapp_name, "webapp")
 
 git "git clone webapp #{app_name}" do
-  repository ca_webapp_info.fetch(:webapp_git_repo, 'https://github.com/harvard-dce/webapp')
-  revision ca_webapp_info.fetch(:webapp_git_revision, 'master')
-  destination '/home/web/sites/#{app_name}'
+  repository ca_webapp_info.fetch(:webapp_git_repo, "https://github.com/harvard-dce/webapp")
+  revision ca_webapp_info.fetch(:webapp_git_revision, "master")
+  destination "/home/web/sites/#{app_name}"
   user 'web'
 end
 
-file '/home/web/sites/#{app_name}/#{app_name}.env' do
-  owner 'web'
-  group 'web'
+file "/home/web/sites/#{app_name}/#{app_name}.env" do
+  owner "web"
+  group "web"
   content %Q|
 export CA_STATS_USER="#{ca_webapp_info[:ca_stats_user]}"
 export CA_STATS_PASSWD="#{ca_webapp_info[:ca_stats_passwd]}"
@@ -29,18 +29,18 @@ export LDAP_BIND_PASSWD="#{ca_webapp_info[:ldap_bind_passwd]}"
 export LOG_CONFIG="#{ca_webapp_info[:log_config]}"
 export FLASK_SECRET="#{ca_webapp_info[:webapp_secret_key]}"
 |
-  mode '600'
+  mode "600"
 end
 
-execute 'create virtualenv' do
-  command '/usr/bin/virtualenv /home/web/sites/#{app_name}/venv'
-  user 'web'
-  creates '/home/web/sites/#{app_name}/venv/bin/activate'
+execute "create virtualenv" do
+  command "/usr/bin/virtualenv /home/web/sites/#{app_name}/venv"
+  user "web"
+  creates "/home/web/sites/#{app_name}/venv/bin/activate"
 end
 
-execute 'install webapp dependencies' do
-  command 'source /home/web/sites/#{app_name}/venv/bin/activate && pip install -r /home/web/sites/#{app_name}/requirements.txt'
-  user 'web'
+execute "install webapp dependencies" do
+  command "source /home/web/sites/#{app_name}/venv/bin/activate && pip install -r /home/web/sites/#{app_name}/requirements.txt"
+  user "web"
 end
 
 cookbook_file "ca-webapp-logrotate.conf" do
