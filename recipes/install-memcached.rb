4d7f6a17150c1013976568444200fd38234c3d1b
nmaekawa <nmaekawa@g.harvard.edu>
2016-04-12 10:05:46 -0400

    matt-1875: memcached and code cleanup

--- recipes/install-memcached.rb
+++ recipes/install-memcached.rb
@@ -4,8 +4,10 @@
 ::Chef::Recipe.send(:include, MhOpsworksRecipes::RecipeHelpers)
 install_package('memcached')
 
-ca_webapp_info = get_ca_webapp_info
+ca_webapp_info = node.fetch(:ca_webapp, {memcached_port: '8008'})
 
-command %Q|/usr/bin/sed -i .bak "s/-p 11211/-p #{ca_webapp_info[:memcached_port]}/g" /etc/memcached.conf|
+execute 'memcached not in default port' do
+  command %Q|/usr/bin/sed -i .bak "s/-p 11211/-p #{ca_webapp_info[:memcached_port]}/g" /etc/memcached.conf|
+end
 
 execute 'service memcached restart'
