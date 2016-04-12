8e3579e08af1d39365c42191190710b5a012709f
nmaekawa <nmaekawa@g.harvard.edu>
2016-04-12 10:46:30 -0400

    matt-1875: fix identation in libraries/default.rb

--- libraries/default.rb
+++ libraries/default.rb
@@ -362,25 +362,27 @@ module MhOpsworksRecipes
     end
   end
 
-  def get_ca_webapp_info
-    node.fetch(
-      :ca_webapp, {
-        ca_stats_user: 'user',
-        ca_stats_passwd: 'passwd',
-        ca_stats_json_url: 'http://ca-status.dceapp.net/ca_stats/ca_stats.json',
-        epipearl_user: 'admin',
-        epipearl_passwd: 'passwd',
-        ldap_host: 'dev-ldap1.dce.harvard.edu',
-        ldap_base_search: 'dc=dce,dc=harvard,dc=edu',
-        ldap_bind_dn: 'cn=user,dc=dce,dc=harvard,dc=edu',
-        ldap_bind_passwd: 'passwd',
-        cadash_secret_key: 'super_secret_really',
-        log_config: 'logging.yaml',
-        memcached_port: '8008'
-      }
-    )
+    def get_ca_webapp_info
+      node.fetch(
+        :ca_webapp, {
+          ca_stats_user: 'user',
+          ca_stats_passwd: 'passwd',
+          ca_stats_json_url: 'http://ca-status.dceapp.net/ca_stats/ca_stats.json',
+          epipearl_user: 'admin',
+          epipearl_passwd: 'passwd',
+          ldap_host: 'dev-ldap1.dce.harvard.edu',
+          ldap_base_search: 'dc=dce,dc=harvard,dc=edu',
+          ldap_bind_dn: 'cn=user,dc=dce,dc=harvard,dc=edu',
+          ldap_bind_passwd: 'passwd',
+          cadash_secret_key: 'super_secret_really',
+          log_config: 'logging.yaml',
+          memcached_port: '8008'
+        }
+      )
+    end
   end
 
+<<<<<<< HEAD
   def get_ca_app_info
     node.fetch(
       :ca_app, {
@@ -397,6 +399,8 @@ module MhOpsworksRecipes
       }
     )
   end
+=======
+>>>>>>> b483e6a... matt-1875: fix identation in libraries/default.rb
 
   module DeployHelpers
     def files_for(node_profile)
